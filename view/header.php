8be3091b48ad2edc027a4404a1c57a1ff8ac93ac
Kai Boon <95kai13@gmail.com>
2023-08-10 23:55:23 +0800

    Update: Add Icon and Respond on copy links

--- view/header.php
+++ view/header.php
@@ -7,7 +7,11 @@
 <head>
     <title>URL Shortener</title>
     <link rel="icon" type="image/png" href="../asset/tinywow_logo_28122598.png" >
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
+    <link href="../dist/output.css" rel="stylesheet">
+    <link href="../asset/font/css/fontawesome.css" rel="stylesheet">
+    <link href="../asset/font/css/solid.css" rel="stylesheet">
+    <link href="../asset/font/css/regular.css" rel="stylesheet">
+    <link href="../asset/font/css/all.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
--- view/shorten_url.php
+++ view/shorten_url.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 
-    include '../view/header.php';
+include '../view/header.php';
 
-    function check_session() {
-        if (isset($_SESSION["shorten_url"])) {
-            return true;
-        } else {
-            return false;
-        }
+function check_session()
+{
+    if (isset($_SESSION["shorten_url"])) {
+        return true;
+    } else {
+        return false;
     }
+}
 ?>
 
 <section class="container max-w-4xl mx-auto mt-5">
@@ -19,18 +20,22 @@
         <div class="mt-10">
             <div class="grid grid-cols-12 justify-stretch">
                 <div class="col-span-10">
-                    <input type="text" class = "border-2 border-slate-200 hover:border-slate-300 focus:hover:border-slate-300 py-2 pl-4 w-full rounded-xl" placeholder="Enter your url" name = "original_url" value = "<?php if(check_session()) { echo $_SESSION["shorten_url"]; } ?>" required>
+                    <input type="text" class="border-2 border-slate-200 hover:border-slate-300 focus:hover:border-slate-300 py-2 pl-4 w-full rounded-xl" placeholder="Enter your url" name="original_url" id="shorten_url"value="<?php if (check_session()) {
+                                                                                                                                                                                                                        echo $_SESSION["shorten_url"];
+                                                                                                                                                                                                                    } ?>" required>
                 </div>
 
                 <div class="col-span-2 text-center">
-                    <button id="copy-btn" class="bg-blue-500 p-2 rounded-xl text-slate-100 px-4" type="submit">Copy</button>
+                    <button id="copy-btn" class="bg-blue-500 p-2 rounded-xl text-slate-100 px-4" type="submit">Copy<i class="fa-solid fa-clipboard-list ml-2" style="color: #ffffff;"></i></button>
                 </div>
             </div>
-            <p class="inline-block mr-2 my-2"> Long URL :</p><span class="text-blue-500"><?php if(check_session()) { echo $_SESSION["original_url"]; } ?></span> 
+            <p class="inline-block mr-2 my-2"> Long URL :</p><span class="text-blue-500"><?php if (check_session()) {
+                                                                                                echo $_SESSION["original_url"];
+                                                                                            } ?></span>
 
             <div class="my-8">
-                <div class="my-3"><a href="../index.php" class="border-2 border-blue-500 bg-blue-500 text-slate-50 rounded-md p-1">Total of clicks of your shortened URL</a></div>
-                <div class="my-3"><a href="../index.php" class="border-2 border-blue-500 bg-blue-500 text-slate-50 rounded-md p-1">Shorten another URL</a></div>
+                <div class="my-3"><a href="../index.php" class="border-2 border-violet-500 bg-violet-500 text-slate-50 rounded-md p-1">Total of clicks of your shortened URL</a></div>
+                <div class="my-3"><a href="../index.php" class="border-2 border-violet-500 bg-violet-500 text-slate-50 rounded-md p-1">Shorten another URL</a></div>
             </div>
         </div>
 
@@ -42,27 +47,38 @@
             <div class="rounded-full bg-violet-500 px-2 py-1 w-[6rem] text-center inline-block text-slate-50">Instagram</div>
             <div class="rounded-full bg-black px-2 py-1 w-[6rem] text-center inline-block text-slate-50">Tiktok</div>
             <div class="rounded-full bg-red-500 px-2 py-1 w-[6rem] text-center inline-block text-slate-50">Youtube</button>
+            </div>
         </div>
-    </div>
 </section>
 
-<?php 
-    include '../view/footer.php';
+<?php
+include '../view/footer.php';
 ?>
 <script>
-    $(function() {
-    var $btnCopy = $('#copy-btn');
+    
+    let btn = document.getElementById('copy-btn');
+    
+    const copyContent = async () => {
+        try {
 
-    $btnCopy.on('click', function() {
-        var clipboard = new Clipboard('#copy-btn');
+            let text = document.getElementById('shorten_url').value;
+            await navigator.clipboard.writeText(text);
+            console.log('Content copied to clipboard');
 
-        clipboard.on('success', function(e) {
-        $btnCopy.text('Copied');
+            document.getElementById("copy-btn").innerHTML = "Copied";
 
-        setTimeout(function() {
-            $btnCopy.text('Copy');
-        }, 2000);
-        });
-    });
-    });
+            //change color of button
+            document.getElementById("copy-btn").classList.remove('bg-blue-500');
+            document.getElementById("copy-btn").classList.add('bg-green-600');
+
+            //change icon of button
+            document.getElementsByClassName("fa-clipboard-list").remove();
+
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    }
+
+    btn.addEventListener("click", copyContent, false);
+
 </script>
